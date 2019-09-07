8bb94fae2e36f03212187e374829a38016ffe658
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-09-07 16:45:45 +0530

    Add typedoc comments

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
 import dgram = require('dgram');
-const {Promise} = require('bluebird');
-import cron = require('node-cron')
+import cron = require('node-cron');
+import bluebird = require('bluebird');
+const {Promise} = bluebird;
 import Anime = require('./anime');
 
+
+/**
+ * Interface for storing AniDB credentials
+ */
 interface Auth {
+  /**
+   * AniDB username of the user
+   */
   username: string;
+  /**
+   * The corresponding password
+   */
   password: string;
+  /**
+   * Version of the API to use
+   */
   protover: number;
+  /**
+   * AniDB registered client name
+   */
   client: string;
+  /**
+   * Version of the client in use
+   */
   clientver: number;
 }
 
+/**
+ * Interface to store AniDB connection parameters; see AniDB constructor
+ */
 interface connOpts {
   url: string;
   port: number;
@@ -20,25 +43,62 @@ interface connOpts {
  * A class for interacting with the UDP API of AniDB
  */
 class AniDB {
+  /**
+   * AniDB session id retrieved from server on login
+   */
   sid: string = '';
+  /**
+   * Promise that resolves to `true` on successful login
+   */
   up: Promise<boolean>;
+  /**
+   * Queue of pending requests
+   *
+   * Example job:
+   * ```
+   * const job = { req: 'PING', callback: (data) => console.log(data) }
+   * ```
+   */
   jobs: Array<{ req: string; callback: Function }> = [];
+  /**
+   * The socket used to make requests and receive data from AniDB
+   */
   reqSock: dgram.Socket;
+  /**
+   * A `node-cron` task that executes requests from `jobs` periodically
+   */
   jobber: cron.ScheduledTask;
+  /**
+   * Optional connection parameters; see constructor
+   */
   opt: connOpts;
 
   /**
    * Create a new AniDB Object and auth to the API
    * @param {Auth} auth Auth object containing AniDB credentials:
-   * -- All fields are required --
+   * ```
+   * //-- All fields are required --
    * username: String;
    * password: String;
    * protover: number; // Specify version of API to use
    * client: String; // Specify name of your client as registered on AniDB
    * clientver: String; // Specify version of client
+   * ```
    * @param {connOpts} opt Optional connections parameters:
+   * ```
    * url: String; // API Endpoint, defaults to api.anidb.net
    * port: number; // AniDB API port, defaults to 9000
+   * ```
+   * Example
+   * ```
+   * const instance = new AniDB({
+   *  username: 'kazumadesu',
+   *  password: 'meguminbestgirl',
+   *  protover: 3
+   *  client: 'perfectlyreasonableclientname',
+   *  clientver: 1,
+   * });
+   * ```
    */
   constructor(auth: Auth, opt?: connOpts) {
     this.makeReq = this.makeReq.bind(this);
